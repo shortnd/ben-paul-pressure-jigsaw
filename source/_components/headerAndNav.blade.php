3b07441c18c150fcf872afcbe0a55791baa4ef6e
shortnd <ococncol@gmail.com>
2018-07-16 23:39:16 -0400

    started and going to test

---
+++ source/_components/headerAndNav.blade.php
@@ -0,0 +1,17 @@
+<header id="header">
+  <div class="top-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="contact-info-wrapper">
+            <a href="tel:+3131313313">(313) 313-3131</a>
+            <a href="mailto:email@email.com">email@email.com</a>
+          </div><!--/.contact-info-wrapper-->
+        </div><!--/.col-md-6-->
+        <div class="col-md-6 pull-right">
+          <a href="facebook.com" target="_blank"><i class="fa fa-facebook"></i><span class="sr-only">facebook link</span></a>
+        </div><!--/.col-md-6.pull-right-->
+      </div><!--/.row-->
+    </div><!--/.container-->
+  </div><!--/.top-bar-->
+</header><!--/#header-->
---
+++ source/index.blade.php
@@ -0,0 +1,7 @@
+@extends('_layouts.master')
+
+@include('_components.headerAndNav')
+
+@section('body')
+<h1>Hello world!</h1>
+@endsection
