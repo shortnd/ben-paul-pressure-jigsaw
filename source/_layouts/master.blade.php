bc4ca0e4021e1711f5cfc9d37944a16d3b6d73bf
shortnd <ococncol@gmail.com>
2018-07-17 00:05:30 -0400

    added some styling and trying another build

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Benjamin Paul Pressure Washing</title>
         <link rel="stylesheet" href="/assets/css/main.css">
     </head>
     <body>
