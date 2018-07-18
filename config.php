bdbe9b686099825df41ae57ab4af5ad2946b9609
shortnd <ococncol@gmail.com>
2018-07-18 00:26:11 -0400

    started working on site

--- config.php
+++ config.php
@@ -4,4 +4,7 @@ return [
     'baseUrl' => '',
     'production' => false,
     'collections' => [],
+    'telephone' => '(313)313-3133',
+    'email' => 'email@email.com',
+    'facebook_link' => 'facebook.com'
 ];
--- source/_components/headerAndNav.blade.php
+++ source/_components/headerAndNav.blade.php
@@ -4,14 +4,46 @@
       <div class="row">
         <div class="col-md-6">
           <div class="contact-info-wrapper">
-            <a href="tel:+3131313313">(313) 313-3131</a>
-            <a href="mailto:email@email.com">email@email.com</a>
+          <a href="tel:+1{{ $page->telephone }}">{{ $page->telephone }}</a>
+          <a href="mailto:{{$page->email}}">{{ $page->email }}</a>
           </div><!--/.contact-info-wrapper-->
         </div><!--/.col-md-6-->
         <div class="col-md-6 text-right">
-          <a href="facebook.com" target="_blank"><i class="fa fa-facebook"></i><span class="sr-only">facebook link</span></a>
+          <a href="https://{{$page->facebook_link}}" target="_blank"><i class="fa fa-facebook"></i><span class="sr-only">facebook link</span></a>
         </div><!--/.col-md-6.text-right-->
       </div><!--/.row-->
     </div><!--/.container-->
   </div><!--/.top-bar-->
+  <div class="bottom-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <a href="/" id="logo">
+            <h1>Benjamin Paul's</h1>
+            <h2>Pressure Washing</h2>
+          </a><!--/#logo-->
+        </div><!--/.col-md-3-->
+        <div class="col-md-9">
+          <div class="text-right">
+            <nav>
+              <ul id="nav">
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/about">About</a>
+                </li>
+                <li>
+                  <a href="/services">Services</a>
+                </li>
+                <li>
+                  <a href="/contact">Contact</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div><!--/.col-md-9-->
+      </div><!--/.row-->
+    </div><!--/.container-->
+  </div><!--/.bottom-bar-->
 </header><!--/#header-->
