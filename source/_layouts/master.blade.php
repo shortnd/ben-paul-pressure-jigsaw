c21c0b3d8024669ba5705fd7923a55fb87d7ac4a
shortnd <ococncol@gmail.com>
2018-07-17 23:39:36 -0400

    corrected errors

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Benjamin Paul Pressure Washing</title>
-        <link rel="stylsheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/layout.css">
     </head>
     <body>
