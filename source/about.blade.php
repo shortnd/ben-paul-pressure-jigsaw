f115118fe44384f668f1fbda985001e7ed498edf
shortnd <ococncol@gmail.com>
2018-07-23 23:45:31 -0400

    added about page

---
+++ source/about.blade.php
@@ -0,0 +1,5 @@
+@extends('_layouts.master')
+
+@section('body')
+  This is a test page for routing and building
+@endsection
--- source/index.blade.php
+++ source/index.blade.php
@@ -3,5 +3,6 @@
 @section('body')
   <div class="container">
     <h1 class="text-center">Site Currently Under Construction</h1>
+    <a href="/about">About</a>
   </div>
 @endsection
