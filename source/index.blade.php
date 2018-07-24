c318a2168e02d3fd8c929a1fc0ce9ae44e9c64b3
shortnd <ococncol@gmail.com>
2018-07-24 00:09:44 -0400

    style2

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.master')
 
 @section('body')
-  <div class="container" style="display:flex; justify-content: center; align-items: center;">
+  <div class="container" style="display:flex; justify-content: center; align-items: center;flex-direction:column;height:100vh;">
     <h1 class="text-center">Site Currently Under Construction</h1>
     <ul>
       <li>
