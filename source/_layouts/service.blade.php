5eaceb57e4cf0f815f9fb0e974a221372778524c
shortnd <ococncol@gmail.com>
2018-07-31 15:40:06 -0400

    added post img to services

--- source/_layouts/service.blade.php
+++ source/_layouts/service.blade.php
@@ -2,6 +2,9 @@
 @section('body')
   <div class="container">
     <h2>{{ $page->title }}</h2>
+    @if ($page->post_img)
+      <img src="{{ $page->assets_prefix }}{{ $page->post_img }}" alt="{{ $page->post_img_alt }}">
+    @endif
     <p>{{ $page->description }}</p>
   </div>
 @endsection
