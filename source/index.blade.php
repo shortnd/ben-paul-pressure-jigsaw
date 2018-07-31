b154ba7249d7cc37874f2402a7ece5fe80bfe917
shortnd <ococncol@gmail.com>
2018-07-31 15:28:25 -0400

    removed links from index

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,13 +3,5 @@
 @section('body')
   <div class="container" style="display:flex; justify-content: center; align-items: center;flex-direction:column;height:100vh;">
     <h1 class="text-center">Site Currently Under Construction</h1>
-    <ul>
-      <li>
-        <a href="/about">About Test</a>
-      </li>
-      <li>
-        <a href="/test">Test</a>
-      </li>
-    </ul>
   </div>
 @endsection
