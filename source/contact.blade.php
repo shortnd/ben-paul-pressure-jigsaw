5fe3e60e579aa08b7a86f5d64fa48a14ee1427a6
shortnd <ococncol@gmail.com>
2018-07-31 00:12:41 -0400

    starting to flesh out contact form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -12,11 +12,18 @@
     </div>
     <div class="form-group"><label for="content">Content</label><textarea class="form-control" id="content" name="content" type="text"></textarea></div>
     <div id="estimate">
-      {[ message ]}
-      <span @click="show">Show</span>
-      <span v-if="showEstimate">TRUE</span>
-      <span v-else="showEstimate">FALSE</span>
-    </div>
+      <span @click="show">{[ message ]}</span>
+      <div v-if="showEstimate" class="estimate">
+        <label for="length">Length: <sub>In feet</sub> <input class="form-control" type="number" name="length" id="length" v-model.number="estimate.length" placeholder="0" /></label>
+        <label for="width">Width: <sub>In feet</sub> <input class="form-control" type="number" name="width" id="width" v-model.number="estimate.width" placeholder="0" /></label>
+        <br>
+        <label for="Percentage">Percentage: {[ estimate.percentage * 100]}% ~ {[ estimate.highPercentage * 100 ]}%</label>
+        <br>
+        <sub>* Percentage may vary depending on job and equipment needed</sub>
+        <br>
+        <h3>Estimated Total: ${[ round(realTotal) ]} ~ ${[ round(highTotal) ]}</h3>
+      </div><!--/.estimate-->
+    </div><!--/#estimate-->
     <button class="btn btn-primary" type="submit" name="submit">Submit</button>
     <button class="btn btn-danger" type="reset" name="reset">Reset</button>
   </form>
@@ -24,16 +31,36 @@
   new Vue({
     delimiters: ["{[","]}"],
     data: {
-      message: 'Hello Vue',
-      showEstimate: false
+      message: 'Show Estimate Calculator +',
+      showEstimate: false,
+      estimate : {
+        length: 0,
+        width: 0,
+        percentage: .45,
+        highPercentage: .60,
+      }
+    },
+    computed: {
+      realTotal: function () {
+        return (this.estimate.length * this.estimate.width) * this.estimate.percentage
+      },
+      highTotal: function () {
+        return (this.estimate.length * this.estimate.width) * this.estimate.highPercentage
+      }
     },
     methods: {
       show: function () {
         if (this.showEstimate === false) {
           this.showEstimate = true
+          this.message = 'Hide Estimate Calculator -'
         } else {
           this.showEstimate = false
+          this.message = 'Show Estimate Calculator +'
         }
+      },
+      round: function(value) {
+        var amount = Math.floor(value * 100) / 100;
+        return amount.toFixed(2)
       }
     }
   }).$mount('#estimate');
