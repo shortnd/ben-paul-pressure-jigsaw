c35035c4a9a7084dc3dafd790ab4855d1c9e87ca
shortnd <ococncol@gmail.com>
2018-08-07 23:57:22 -0400

    updating images folder now

--- source/_layouts/service.blade.php
+++ source/_layouts/service.blade.php
@@ -1,7 +1,7 @@
 @extends('_layouts.inner')
 @section('body')
     @if ($page->post_img)
-      <img src="{{ $page->assets_prefix }}{{ $page->post_img }}" alt="{{ $page->post_img_alt }}">
+      <img src="{{ $page->post_img }}" alt="{{ $page->post_img_alt }}">
     @endif
     @yield('content')
 @endsection
